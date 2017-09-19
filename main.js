f3ae49e8a201e39a745aae3d37d57dc12b0c47ea
Vichkarichka <Vichkarichka@gmail.com>
2017-09-19 19:56:04 +0300

    add check for a draw

--- main.js
+++ main.js
@@ -1,44 +1,54 @@
 var stroke = ['x', 'o'];
 var player = 0;
 var mas = [null, null, null, null, null, null, null, null, null];
-var winnerMas = ['012','345','678','036','147','258','048','246'];
+var winnerMas = ['012', '345', '678', '036', '147', '258', '048', '246'];
 var isFinished = false;
+var count = 0;
 
 window.onload = function() {
   $(".field").children("div").click(
-    function(event){
+    function(event) {
       writeState(event)
     });
-    $(".button").click(function(){
-      restart();
-    });
-}
-function restart(){
-  $(".field").children("div").html("");
- mas = [null, null, null, null, null, null, null, null, null];
- isFinished = false;
+  $(".button").click(function() {
+    restart();
+  });
 }
 
-function isWinner(){
-  winnerMas.forEach(function (item, i, arr){
+function restart() {
+  $(".field").children("div").html("");
+  mas = [null, null, null, null, null, null, null, null, null];
+  isFinished = false;
+  count = 0;
+}
+
+function isWinner() {
+  winnerMas.forEach(function(item, i, arr) {
     var elementMas = item.split("");
-	if(mas[elementMas[0]] == mas[elementMas[1]]){
-		if(mas[elementMas[1]] == mas[elementMas[2]]){
-			if(mas[elementMas[0]] != null){
-				alert("Win is " + stroke[player]);
-                isFinished = true;
-                return true;
-			}
-		}
+    if (mas[elementMas[0]] == mas[elementMas[1]]) {
+      if (mas[elementMas[1]] == mas[elementMas[2]]) {
+        if (mas[elementMas[0]] != null) {
+          alert("Win is " + stroke[player]);
+          isFinished = true;
+          count = 0;
+          return true;
+        }
+      }
     }
   });
+  if(count == 9){
+    alert("Вилочки вологодские");
+    isFinished = true;
+    count = 0;
+    return true;
+  }
   return false;
 }
 
-function placeElement(findId){
-    $("#" + findId).text(stroke[player]);
-    mas[findId] = stroke[player];
-    isWinner();
+function placeElement(findId) {
+  $("#" + findId).text(stroke[player]);
+  mas[findId] = stroke[player];
+  isWinner();
 }
 
 function writeState(event) {
@@ -46,6 +56,7 @@ function writeState(event) {
   if (mas[findId] != null || isFinished) {
     return 1;
   }
+  count ++;
   if (player == 0) {
     placeElement(findId);
     player = 1;
